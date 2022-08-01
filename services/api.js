f253d4843de88ae45e61eae80ae7387669c050bb
Roy Kachani <roy.kachani@gmail.com>
2022-08-01 02:20:46 -0300

    fix method

--- services/api.js
+++ services/api.js
@@ -3,13 +3,16 @@ import axios from 'axios';
 const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export const getAll = async () => {
-  const { data } = await axios(baseUrl);
+  const { data } = await axios.get(baseUrl);
 
   return data;
 };
 
 export const getModelById = async (id) => {
-  const { data } = await axios(`${baseUrl}${id}`);
+  const { data } = await axios.get({
+    method: 'GET',
+    url: `${baseUrl}/${id}`,
+  });
 
   return data;
 };
