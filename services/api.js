ff6e558e9a130b84fd05ef4189f1b7cd509e54e6
Roy Kachani <roy.kachani@gmail.com>
2022-08-01 02:11:19 -0300

    fix axios metodo

--- services/api.js
+++ services/api.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export const getAll = async () => {
-  const { data } = await axios.get(baseUrl);
+  const { data } = await axios(baseUrl);
 
   return data;
 };
 
 export const getModelById = async (id) => {
-  const { data } = await axios.get(`${baseUrl}${id}`);
+  const { data } = await axios(`${baseUrl}${id}`);
 
   return data;
 };
